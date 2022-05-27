ec3fc2ad99f71be6cfcaf92de617135be9ec13f3
Serhii <maystrenko8@gmail.com>
2022-05-27 13:59:08 +0300

    deploy and verify smart-contract

--- contracts/RoboPunksNFT.sol
+++ contracts/RoboPunksNFT.sol
@@ -2,22 +2,22 @@
 
 pragma solidity ^0.8.4;
 
-import '@openzzepelin/contracts/token/ERC21/ERC21.sol';
-import '@openzzepelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RopoPunkNFT is ERC721 , Ownable {
+contract RoboPunksNFT is ERC721 , Ownable {
     uint256 public mintPrice;
     uint256 public totalSupply;
     uint256 public maxSupply;
     uint256 public maxPerWallet;
-    bool public isPublicMintEnable;
+    bool public isPublicMintEnabled;
     string internal baseTokenUri;
     address payable public withdrawWallet;
     mapping(address => uint256) public walletMints;
 
-    constructor() payable ERC721('RoboPunkNFT', 'RP') {
+    constructor() payable ERC721('RoboPunksNFT', 'RP') {
         mintPrice = 0.02 ether;
-        totalySupply = 0;
+        totalSupply = 0;
         maxSupply = 1000;
         maxPerWallet = 3;
         // set withdraw wallet address
@@ -31,7 +31,7 @@ contract RopoPunkNFT is ERC721 , Ownable {
         baseTokenUri = baseTokenUri_;
     }
 
-    function tokenURI(uint256 tokenID_) public view override returns (string memory) {
+    function tokenURI(uint256 tokenId_) public view override returns (string memory) {
         require(_exists(tokenId_), 'Token does not exist');
         return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), ".json"));
     }
@@ -45,10 +45,14 @@ contract RopoPunkNFT is ERC721 , Ownable {
         require(isPublicMintEnabled, 'minying not enabled');
         require(msg.value == quantity_ * mintPrice, 'wrong mint value');
         require(totalSupply +  quantity_ <= maxSupply, 'sold out');
-        require(walleMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet');
+        require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet');
+
+    for (uint256 i = 0; i < quantity_; i++) {
+        uint256 newTokenId = totalSupply + 1;
+        totalSupply++;
+        _safeMint(msg.sender, newTokenId);
+        }
+
     }
-    
-
-
 
 }
