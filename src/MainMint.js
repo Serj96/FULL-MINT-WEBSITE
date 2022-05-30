d51f1173cd6cec5404cb8b878aba5aeb6e4d7278
Serhii <maystrenko8@gmail.com>
2022-05-30 16:55:26 +0300

    corrected errors

--- src/MainMint.js
+++ src/MainMint.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { ethers, BigNumber} from 'ethers';
+import { Box, Button, Flex, Input, Text } from '@chakra-ui/react';
 import roboPunksNFT from './RoboPunksNFT.json';
 
 const roboPunksNFTAddress = "0x85f6957547f3e52841A816802c01251d833C51b3";
@@ -18,7 +19,9 @@ const MainMint = ({ accounts, setAccounts}) => {
                 signer
             );
             try{
-                const response = await contract.mint(BigNumber.from(mintAmount) );
+                const response = await contract.mint(BigNumber.from(mintAmount), {
+                    value: ethers.utils.parseEther((0.02 * mintAmount).toString()), 
+                });
                 console.log('response: ', response);
             } catch (err) {
                 console.log("error: ", err)
@@ -37,22 +40,91 @@ const MainMint = ({ accounts, setAccounts}) => {
     };
 
     return (
-        <div>
-            <h1>RoboPunks</h1>
-            <p>It's 2078. Can the RoboPunks NFT save humans from destructive rampant NFT speculation? Mint RoboPunks to find out.</p>
+        <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
+            <Box width="520px">
+                <div>
+                <Text fontSize="48px" textShadow="0 5px #000000">
+                    RoboPunks
+                    </Text>
+                <Text
+                 fontSize="30px"
+                 letterSpacing="-5.5%"
+                 fontFamily="VT323"
+                 textShaow="0 2px 2px #000000"
+                > 
+                 It's 2078. Can the RoboPunks NFT save humans from destructive rampant NFT speculation? Mint RoboPunks to find out.
+                 </Text>
+                </div>
+                
             {isConnected ? (
                 <div>
-                    <div>
-                        <button onClick={handleDecrement}>-</button>
-                        <input type="number" value={mintAmount} />
-                        <button onClick={handleIncrement}>+</button>
-                    </div>
-                    <button onClick={handleMint}>Mint Now</button>
+                    <Flex justify="center" align="center">
+                        <Button
+                        backgroundColor="#D6517D"
+                        borderRadius="5px"
+                        boxShadow="0px 2px 2px 1px #0F0F0F"
+                        color="white"
+                        cursor="pointer"
+                        fontFamily="inherit"
+                        padding="15px"
+                        marginTop="10px"
+                        onClick={handleDecrement}
+                        >
+                            -
+                        </Button>
+                        <Input 
+                        readOnly
+                        fontFamily="inherit"
+                        width="100px"
+                        hight="40px"
+                        textAlign="centre"
+                        paddingLeft="19px"
+                        marginTop="10px"
+                        type="number" 
+                        value={mintAmount} 
+                        />
+                        <Button
+                        backgroundColor="#D6517D"
+                        borderRadius="5px"
+                        boxShadow="0px 2px 2px 1px #0F0F0F"
+                        color="white"
+                        cursor="pointer"
+                        fontFamily="inherit"
+                        paddingLeft="15px"
+                        marginTop="10px"
+                        onClick={handleIncrement}
+                        >
+                            +
+                        </Button>
+                    </Flex>
+                    <Button
+                        backgroundColor="#D6517D"
+                        borderRadius="5px"
+                        boxShadow="0px 2px 2px 1px #0F0F0F"
+                        color="white"
+                        cursor="pointer"
+                        fontFamily="inherit"
+                        paddingLeft="15px"
+                        marginTop="10 px" 
+                        onClick={handleMint}
+                        >
+                        Mint Now
+                        </Button>
                 </div>
             ) : (
-                <p>You must be connected to Mint.</p>
+                <Text
+                 marginTop="70px"
+                 fontSize="30px"
+                 letterSpacing="-5.5%"
+                 fontFamily="VT323"
+                 textShaow="0 3px #000000"
+                 color="#D6517D"
+                >
+                    You must be connected to Mint.
+                    </Text>
             )}
-        </div>
+        </Box>
+        </Flex>
         );
     };
 
